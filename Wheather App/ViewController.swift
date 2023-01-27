c5f111fea537a5c6a3e4058d22f72946ca880925
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-01-27 16:18:38 +0300

    all components are connected

--- Wheather App/ViewController.swift
+++ Wheather App/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var konumLabel: UILabel!
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var tempetureLabel: UILabel!
+    @IBOutlet weak var humindtyLabel: UILabel!
+    @IBOutlet weak var rainLabel: UILabel!
+    @IBOutlet weak var refreshButtonOut: UIButton!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func resreshButtonPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
