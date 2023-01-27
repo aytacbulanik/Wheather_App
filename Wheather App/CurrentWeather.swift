609a3c32fd72c1d37b5d3f6913c30267f8a13199
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-01-27 17:21:56 +0300

    weather models created

---
+++ Wheather App/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Wheather App
+//
+//  Created by MacBookPro on 27.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+    let tempeture : Double
+    let precipProbability : Double
+    let summary : String
+    let humidity : Double
+    let icon : String
+    let iconImage : UIImage
+}
---
+++ Wheather App/CurrentWeatherModel.swift
@@ -0,0 +1,25 @@
+//
+//  CurrentWeatherModel.swift
+//  Wheather App
+//
+//  Created by MacBookPro on 27.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeatherModel {
+    let summary : String
+    let icon : UIImage
+    let tempeture : String
+    let humidity : String
+    let precipitationProboilitiy : String
+    
+    init(data : CurrentWeather) {
+        self.summary = data.summary
+        self.precipitationProboilitiy = "\(data.precipProbability * 100)"
+        self.tempeture = "\(Int(data.tempeture))"
+        self.humidity = "% \(Int(data.humidity * 100))"
+        self.icon = data.iconImage
+    }
+}
