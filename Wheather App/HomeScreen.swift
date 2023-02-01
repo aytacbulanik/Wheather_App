3ed7229169b23e5459a524062fb13fe315ebcbea
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-02-01 23:30:13 +0300

    json parse is true

--- Wheather App/HomeScreen.swift
+++ Wheather App/HomeScreen.swift
@@ -17,12 +17,21 @@ class HomeScreen: UIViewController {
     }
     
     func getUrlData() {
-        let url = URL(string: "https://api.apilayer.com/fixer/symbols?apikey=VAfbnBSmAnnGK0KUkt8h1zDLRQ6LqOoi")
+        let anotherUrl = URL(string: "http://data.fixer.io/api/latest?access_key=44406cc034b7e6610b5c5647cc9fe2d4")
+        // let url = URL(string: "https://api.apilayer.com/fixer/symbols?apikey=VAfbnBSmAnnGK0KUkt8h1zDLRQ6LqOoi")
         let session = URLSession.shared
-        let task = session.dataTask(with: url!) { data, response, error in
-            print(data)
-            print(response)
-            print(error)
+        let task = session.dataTask(with: anotherUrl!) { data, response, error in
+            if let data = data {
+                do {
+                    let jsonResponse  = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    DispatchQueue.main.async {
+                        print(jsonResponse)
+                    }
+                }catch {
+                    print(error.localizedDescription)
+                }
+                
+            }
         }
         task.resume()
     }
