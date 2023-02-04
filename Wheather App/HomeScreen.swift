670465eaae99595785d60397c657e09b7fc905fa
Aytaç Bulanık <aytacbulanik@hotmail.com>
2023-02-04 22:01:21 +0300

    textField config added

--- Wheather App/HomeScreen.swift
+++ Wheather App/HomeScreen.swift
@@ -12,19 +12,40 @@ class HomeScreen: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var searchTextField: UITextField!
-    
     @IBOutlet weak var statusLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
+        searchTextField.delegate = self
+        let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
+        view.addGestureRecognizer(tap)
     }
-    
+    //klavyeyi kapatacak
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
+    }
+    // textfiled içerisinin boşaltacak
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    //textfield boş kontrolü yapacak
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            textField.placeholder = "Type a someThing"
+            return true
+        } else {
+            
+            return true
+        }
+    }
     @IBAction func serachButtonPressed(_ sender: UIButton) {
         
     }
-    
-   
+    // ekranda herhangi bir yere dokununca klavye kapatılacak
+    @objc func closeKeyboard() {
+        view.endEditing(true)
+    }
 
 }
