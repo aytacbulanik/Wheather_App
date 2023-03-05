a81c7cf4f40de388a57aab94d72e73394347aacf
Aytac Bulanik <aytacbulanik@hotmail.com>
2023-03-05 21:30:49 +0300

    design completed

--- Wheather App/Controller/HomeScreen.swift
+++ Wheather App/Controller/HomeScreen.swift
@@ -7,48 +7,29 @@
 
 import UIKit
 
-class HomeScreen: UIViewController, UITextFieldDelegate {
+class HomeScreen: UIViewController {
 
     
     
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var statusLabel: UILabel!
+    
     let weatherManager = WeatherMenager()
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        searchTextField.delegate = self
-        let tap = UITapGestureRecognizer(target: self, action: #selector(closeKeyboard))
-        view.addGestureRecognizer(tap)
+        
     }
     //klavyeyi kapatacak
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.endEditing(true)
-        return true
-    }
+   
     // textfiled içerisinin boşaltacak
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        if let city = textField.text {
-        weatherManager.fetchWeather(cityName: city)
-        }
-        textField.text = ""
-    }
+   
     //textfield boş kontrolü yapacak
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        if textField.text == "" {
-            textField.placeholder = "Type a someThing"
-            return true
-        } else {
-            
-            return true
-        }
-    }
+    
     @IBAction func serachButtonPressed(_ sender: UIButton) {
         
     }
     // ekranda herhangi bir yere dokununca klavye kapatılacak
-    @objc func closeKeyboard() {
-        view.endEditing(true)
-    }
+    
 
 }
